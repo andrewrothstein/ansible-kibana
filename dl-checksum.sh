197c17b20c0e91e956b2a7d65219cb9bedc939be
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-11-09 07:07:57 -0500

    up to v6.4.3. add a dl-checksums.sh

---
+++ dl-checksum.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+VER=6.4.3
+DIR=~/Downloads
+MIRROR=https://artifacts.elastic.co/downloads/kibana
+
+dl()
+{
+    OS=$1
+    PLATFORM=$2
+    ZIPTYPE=$3
+    FILE=kibana-$VER-$OS-$PLATFORM.$ZIPTYPE
+    wget -O $DIR/$FILE $MIRROR/$FILE
+}
+
+dl linux x86_64 tar.gz
+dl darwin x86_64 tar.gz
+dl windows x86_64 zip
+sha256sum $DIR/kibana-$VER-*
+
