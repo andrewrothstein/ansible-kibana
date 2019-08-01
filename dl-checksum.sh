8471a5d45f82dbf5867ac8793e018ced8518594e
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-08-01 16:15:49 -0500

    up to v7.3.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env sh
-VER=7.2.0
+VER=7.3.0
 DIR=~/Downloads
 MIRROR=https://artifacts.elastic.co/downloads/kibana
 
 dl()
 {
     OS=$1
-    PLATFORM=$2
+    ARCH=$2
     ZIPTYPE=$3
-    SHA512=kibana-$VER-$OS-$PLATFORM.$ZIPTYPE.sha512
+    PLATFORM=${OS}-${ARCH}
+    SHA512=kibana-$VER-$PLATFORM.$ZIPTYPE.sha512
     URL=$MIRROR/$SHA512
-    printf "  # %s\n" $URL
-    printf "  %s: sha512:%s\n" "$OS-$PLATFORM" `curl -s $MIRROR/$SHA512 | awk '{print $1}'`
+    printf "    # %s\n" $URL
+    printf "    %s: sha512:%s\n" $PLATFORM `curl -s $MIRROR/$SHA512 | awk '{print $1}'`
 }
 
-printf "'%s':\n" $VER
+printf "  '%s':\n" $VER
 dl linux x86_64 tar.gz
 dl darwin x86_64 tar.gz
 dl windows x86_64 zip
