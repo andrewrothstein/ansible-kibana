6601f459c9aa97dbb8bd61ceb21fb1236096296a
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-02 19:57:02 -0500

    up to v7.4.2. restated checksums for old versions.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,23 +1,46 @@
 #!/usr/bin/env sh
-VER=7.3.0
-DIR=~/Downloads
-MIRROR=https://artifacts.elastic.co/downloads/kibana
 
-dl()
-{
-    OS=$1
-    ARCH=$2
-    ZIPTYPE=$3
-    PLATFORM=${OS}-${ARCH}
-    SHA512=kibana-$VER-$PLATFORM.$ZIPTYPE.sha512
-    URL=$MIRROR/$SHA512
-    printf "    # %s\n" $URL
-    printf "    %s: sha512:%s\n" $PLATFORM `curl -s $MIRROR/$SHA512 | awk '{print $1}'`
+DIR=~/Downloads
+APP=kibana
+MIRROR=https://artifacts.elastic.co/downloads/${APP}
+
+dl() {
+    ver=$1
+    os=$2
+    arch=$3
+    archive_type=$4
+    local platform="${os}-${arch}"
+    local url=https://artifacts.elastic.co/downloads/${APP}/${APP}-${ver}-${platform}.${archive_type}.sha512
+    printf "    # %s\n" $url
+    printf "    '%s': sha512:%s\n" $platform $(curl -sSL $url | awk '{print $1}')
 }
 
-printf "  '%s':\n" $VER
-dl linux x86_64 tar.gz
-dl darwin x86_64 tar.gz
-dl windows x86_64 zip
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver linux x86_64 tar.gz
+    dl $ver darwin x86_64 tar.gz
+    dl $ver windows x86_64 zip
+}
 
+#dl_ver ${1:-7.4.2}
+dl_ver 6.0.1
+dl_ver 6.1.0
+dl_ver 6.1.1
+dl_ver 6.1.2
+dl_ver 6.1.3
+dl_ver 6.2.1
+dl_ver 6.2.2
+dl_ver 6.2.3
+dl_ver 6.2.4
+dl_ver 6.4.1
+dl_ver 6.4.2
+dl_ver 6.4.3
+dl_ver 6.5.1
+dl_ver 6.5.2
+dl_ver 6.6.2
+dl_ver 6.7.0
+dl_ver 7.0.0
+dl_ver 7.1.0
+dl_ver 7.1.1
 
